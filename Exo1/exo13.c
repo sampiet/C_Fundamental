8501997f70c4278a118592d3130b80cee63af93d
basumbintwali <basumbintwali@gmail.com>
2017-10-05 12:09:56 +0200

    exercice13 C -Fund

---
+++ Exo1/exo13.c
@@ -0,0 +1,21 @@
+
+#include <stdio.h>
+ 
+int main(int argc, char *argv[] )
+{
+   
+
+ for (int i=1 ; i < argc; i++)
+{
+ 
+
+printf(" name of the programme %s\n",argv[i] ) ;
+
+}
+
+
+
+
+
+    return 0;
+}
