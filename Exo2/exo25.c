e1a60e0d24d7613b466a2d9635f32bcde0b0452e
basumbintwali <basumbintwali@gmail.com>
2017-10-09 13:20:12 +0200

    excercice 2.5

---
+++ Exo2/exo25.c
@@ -0,0 +1,124 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+
+
+
+int   numb_div(int  numb) ;
+int  *divid_numb(int  numb) ;
+
+
+
+int main(int argc, char *argv[] )
+
+{ 
+	char *oprd_l =argv[1];
+	
+	int numb = (int)strtol(oprd_l, NULL, 10);
+	int cpt=numb_div(numb) ;
+	int *p ; 
+
+	p= divid_numb(numb); 
+	int sum_div =numb;
+
+
+	for(int i =0 ; i<cpt ; i++)
+	{
+	     sum_div+= *p ;
+
+         p++;
+
+	     
+
+
+
+
+	}
+
+	// check if the number is perfect number 
+       
+ 
+	if(sum_div==2*numb)
+	{
+
+		printf("The number is a perfect") ;
+    }
+
+    else
+
+    	printf("The number is not a perfect number") ;
+
+
+
+
+
+
+}
+
+
+
+
+
+/*
+Function to computer the  number of divider number
+*/
+
+
+int  numb_div(int  numb)
+{
+	int cpt=0;
+	
+
+	for(int i=1; i<numb; i++)
+	{
+		if((numb %i)==0)
+		{
+			cpt++ ;
+		}
+			
+
+			
+
+       }
+       return cpt ;
+
+
+}
+
+/*
+Function to computer the different divider numbers
+*/
+
+int  *divid_numb(int  numb)
+{
+
+	int cpt = numb_div(numb) ;
+        
+    int  *array_div=malloc(sizeof(int)*cpt) ;
+    int j=0;
+	
+	for(int i=1; i<numb; i++)
+	{
+		if((numb %i)==0)
+		{
+			array_div[j]= i ;
+			j++;
+
+		}
+
+			
+     }
+
+
+
+
+    return array_div ;
+
+
+}
+
+
+
+
+
