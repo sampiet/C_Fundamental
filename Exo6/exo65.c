2e5635bc23cfc6e071a0c66875a759f10f02a2a4
basumbintwali <basumbintwali@gmail.com>
2017-10-12 19:04:00 +0200

    add exercise 6.5

--- Exo6/exo65.c
+++ Exo6/exo65.c
@@ -6,59 +6,55 @@
 #include <ctype.h>
 int  string_length(const char *str1 ) ;
 bool string_equal(const char *str1,const char *str2, int size ) ;
+
 int main(int argc, char *argv[] )
-{   
-   
-   char *oprd_l1 =argv[1];
-   char *oprd_l2 =argv[2];
-   int size = string_length(oprd_l1)  ;
-   int cpt = string_length(oprd_l1) ;
-   if(string_length(oprd_l1)!=string_length(oprd_l2))
-   {
 
-     printf("Length of the string not equal ");
-   }
-   else 
-       {
-       	  if(string_equal(oprd_l1, oprd_l2, size))
-       	    { 
-               printf("Strings are equal") ;
-            }
-          else
-              {
-                printf("Strings are not equal") ;
-              }
-
-       }
+{
+  char *oprd_l1 =argv[1];
+  char *oprd_l2 =argv[2] ;
+  int size = string_length(oprd_l1)  ;
+  if(string_length(oprd_l1)!=string_length(oprd_l2))
+  {
+    printf("Length of the string not equal ");
+  }
+  else
+  {
+    if(string_equal(oprd_l1, oprd_l2, size))
+    { 
+      printf("Strings are equal") ;
+    }
+    else
+    {
+      printf("Strings are not equal") ;
+    }
+  }
 
    
 }
 
 bool string_equal(const char *str1,const char *str2, int size) 
 {
-	 
-    bool val = true  ; 
-	for (int i=0 ;i <size; i++)
-	{
-		if(*(str1+i)!=*(str2+i))
-		{
-			val= false ;
+  bool val = true  ; 
+  for (int i=0 ;i <size; i++)
+  {
+    if(*(str1+i)!=*(str2+i))
+    {
+      val= false ;
+    }
 
-		}
-
-	}
-	return val ;
+  }
+  return val ;
 }
-
 
 int  string_length(const char *str1 ) 
 {
-	int cpt= 0 ;
-	while(*(str1+cpt)!='\0')
-	{
-		cpt ++ ;
-		
-	}
-	return cpt ;
-
+  int cpt= 0 ;
+  while(*(str1+cpt)!='\0')
+  {
+    cpt ++ ;
+  }
+  return cpt ;
 }
+
+
+
