f1b6d5b1e8c2842f8009eaab51303b3242973752
basumbintwali <basumbintwali@gmail.com>
2017-10-14 00:13:12 +0200

    add exercise 6.4 : strlen of parameters by pointer

---
+++ Exo6/exo64.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+#include <math.h>
+#include <ctype.h>
+
+int main(int argc, char *argv[] )
+{    
+	
+	for(int i = 1 ; i<argc ; i ++)
+	{
+	  printf("String %d\n", (int)strlen(*(argv +i))) ;	
+	  
+	}
+    return 0 ;
+
+}
