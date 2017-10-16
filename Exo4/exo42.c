e410b4b683097066956be441fb48dfd9e856dea6
basumbintwali <basumbintwali@gmail.com>
2017-10-16 10:11:07 +0200

    add exercise 4.2 : Input

---
+++ Exo4/exo42.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int cmpfunc (const void * a, const void * b) 
+{
+   return ( *(int*)a - *(int*)b );
+}
+
+int main(int argc, char *argv[] )
+{
+    int numb =10  ;
+    int array[numb] ;
+    for(int i=0; i<numb; i++)
+    {
+	printf("Enter the number ") ;
+	scanf("%d", &array[i]) ;
+    }
+    qsort(array, numb, sizeof(int), cmpfunc);
+    printf("The maximum number of the array is %d ", array[9]) ;
+
+}
+
+
+	
+	
+
+
