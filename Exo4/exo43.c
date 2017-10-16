cc35be320febadf4341eeb05194e8a60e17cc991
basumbintwali <basumbintwali@gmail.com>
2017-10-16 10:32:46 +0200

    add exercise 4.3 :Environment

---
+++ Exo4/exo43.c
@@ -0,0 +1,16 @@
+
+
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+    printf("test\n");
+    const char* s = getenv("USER");
+    printf("USER :%s\n", s );
+    const char* s2 = getenv("PATH");
+    printf("PATH :%s\n", s2 );
+    const char* s3 = getenv("HOME");
+    printf("HOME :%s\n", s3);
+
+
+}
