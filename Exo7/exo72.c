1ce9aad53e40d97c5aea7b6da3b0dc5922a94f3c
basumbintwali <basumbintwali@gmail.com>
2017-10-18 10:48:35 +0200

    exercise 7.2 : change the code by deleting size array paramter

--- Exo7/exo72.c
+++ Exo7/exo72.c
@@ -2,26 +2,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-
-int max_array(int array[], int size) ;
-
+int max_array(int array[]) ;
 int main (int argc, char **argv)
 {
-  int array[4]={3, 1, 5,6} ;// Test code
-  int size = (int)sizeof(array)/(int)sizeof(int) ;
-  printf("The max of the array is %d\n", max_array(array, size)) ;
+  int array[4]={3, 1, 5,6};
+  printf("The max of the array is %d\n", max_array(array)) ;
 }
-
-int max_array(int array[], int size)
+int max_array(int array[])
 {
-  int  a = array[0] ;
-  for(int i=1; i<size; i++)
-  {
-     if(a<array[i])
-     {
-        a=array[i] ;
-     }
-  } 
-  return a  ;
-}
+  int size = (int)sizeof(array)/(int)sizeof(int);
+  return size ;
+} 
+
 
