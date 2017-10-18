ab4e382d98fe8efa04938a4f85cf87416e95ea01
basumbintwali <basumbintwali@gmail.com>
2017-10-18 11:18:58 +0200

    improve indentation of the exercise 6.6

--- Exo6/exo66.c
+++ Exo6/exo66.c
@@ -5,84 +5,79 @@
 #include <math.h>
 #include <ctype.h>
 #define BUFFER_SIZE   11  
-
-char   buffer[BUFFER_SIZE]={'f', 'f', 'f',  'f', 'f', 'f', 'f', 'f', 'f', 'f', 'f'} ;
+char   buffer[BUFFER_SIZE]={' ', ' ', ' ',  ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '} ;
 void   write(char a) ;
 char   read(int position) ;
 char   *pointer_start ;
 char   *pointer_end ;
 int     count_fill ;
-
 int main(int argc, char *argv[] )
-
 {
-
 	count_fill= 0 ;
 	pointer_start= &buffer[10];
 	pointer_end  = &buffer[10];
 	char  value ;
 	char array[]="abcdeghikkki";//for testing write function
 	int size     =strlen(array) ;
-        for(int i=0 ; i<size  ;i++)
+	for(int i=0 ; i<size  ;i++)
 	{
-	   write(array[i])  ;
-        }
-        printf("The   content of the buffer is %s\n", buffer)  ;
+		write(array[i])  ;
+	}
+	printf("The   content of the buffer is %s\n", buffer)  ;
 }
-
 void   write(char value )
-{ 
-	
-    
+{
 	if(count_fill<BUFFER_SIZE)
 	{
-		
 		if((pointer_end>=pointer_start) && (pointer_end< &buffer[BUFFER_SIZE]))
 		{
 			*pointer_end=value ;
 			pointer_end++ ;
 			count_fill++ ;
-                }
-		else
+		}
+		else 
+		{
 			if(pointer_end==&buffer[BUFFER_SIZE])
 			{       
-                               *pointer_end=value ;
+				*pointer_end=value ;
 				pointer_end=&buffer[0];
 				count_fill++ ;
-                        }
+			}
 			else
 			{
-			       *pointer_end=value ;
+				*pointer_end=value ;
 				pointer_end++ ;
 				count_fill++ ;
-                       }
-         }
-         else
-		{   
-			if(pointer_start==pointer_end)
+			}
+	    }
+	}
+	else
+	{   
+		if(pointer_start==pointer_end)
+		{
+			*pointer_start=  value ;
+			pointer_start ++ ;
+		}
+		else
+		{
+			if(pointer_start<&buffer[BUFFER_SIZE])
 			{
-				*pointer_start=  value ;
-				pointer_start ++ ;
-	                }
+				*pointer_start = value ;
+				pointer_end  = pointer_start;
+				pointer_start ++;
+			}
 			else
 			{
-				if(pointer_start<&buffer[BUFFER_SIZE])
+				if(pointer_start==&buffer[BUFFER_SIZE])
 				{
 					*pointer_start = value ;
+					pointer_start = &buffer[0] ;
 					pointer_end  = pointer_start;
 					pointer_start ++ ;
 				}
-				else
-					if(pointer_start==&buffer[BUFFER_SIZE])
-					{
-						*pointer_start = value ;
-						pointer_start = &buffer[0] ;
-						pointer_end  = pointer_start;
-						pointer_start ++ ;
-					}
-				}
-
 			}
 		}
+	}
+}
 
 
