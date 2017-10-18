a5bf0c9d232d56ce4e66d63b0ba9eedb2631ceb7
basumbintwali <basumbintwali@gmail.com>
2017-10-18 13:43:53 +0200

    exercise 6.4 : improve the correctness of the exercise

--- Exo6/exo64.c
+++ Exo6/exo64.c
@@ -4,15 +4,22 @@
 #include <stdbool.h>
 #include <math.h>
 #include <ctype.h>
-
+int string_len(char *ptr);
 int main(int argc, char *argv[] )
-{    
-	
+{
 	for(int i = 1 ; i<argc ; i ++)
 	{
-	  printf("String %d\n", (int)strlen(*(argv +i))) ;	
-	  
+		printf("The string length is %d for the argument %d \n", (int) string_len(*(argv +i)), i) ;	
 	}
-    return 0 ;
-
+	return 0 ;
+}
+int string_len(char *ptr)
+{
+	int cpt=0;
+	while(ptr[0]!='\0')
+	{
+		cpt++ ;
+		ptr++ ;
+	}
+	return cpt ;
 }
