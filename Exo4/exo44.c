1a3cfcd804f138dcd451709717fae3c4cb3ad2ee
basumbintwali <basumbintwali@gmail.com>
2017-10-20 12:21:34 +0200

    exercise : add a while loop to improve the correctness

--- Exo4/exo44.c
+++ Exo4/exo44.c
@@ -1,12 +1,7 @@
 #include <stdio.h>
-
 #include <stdlib.h>
-
 #include <time.h> 
-
-
 int main(void)
-
 {
     srand(time(NULL));
     printf("Please enter the max random number");
@@ -19,6 +14,31 @@ int main(void)
     printf("Please, guess the random number \n");
     int guess ;
     scanf("%d", &guess);
+    int numb_guess =1;
+    while(guess!=rand_numb)
+    {
+        if(guess <rand_numb)
+        {
+            printf("The number entered is low \n");
+            printf("Please enter a new number \n");
+            scanf("%d", &guess);
+            numb_guess ++ ;
+        
+        }
+        else 
+        {
+            if(guess>rand_numb)
+            {
+                printf("The number entered is high \n");
+                printf("Please enter a new number \n");
+                scanf("%d", &guess);
+                numb_guess ++ ;
+            }
+        }
+    }
+    printf("The amount of number guess is %d\n", numb_guess);
+
+    
     if(guess==rand_numb)
     {
     	printf("you guessed the good number\n");
