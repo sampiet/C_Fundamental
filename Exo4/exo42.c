94c2e747e4c31ea16295b5d86c683d8a2802f1b2
basumbintwali <basumbintwali@gmail.com>
2017-10-29 21:31:55 +0100

    exercose 4.2 : improve indentation

--- Exo4/exo42.c
+++ Exo4/exo42.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
 int cmpfunc (const void * a, const void * b) 
 {
    return ( *(int*)a - *(int*)b );
 }
-
 int main(int argc, char *argv[] )
 {
     int numb =10  ;
@@ -18,7 +16,6 @@ int main(int argc, char *argv[] )
     }
     qsort(array, numb, sizeof(int), cmpfunc);
     printf("The maximum number of the array is %d ", array[9]) ;
-
 }
 
 
