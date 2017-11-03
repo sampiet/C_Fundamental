eb7e37a76d3f66a6010cc98983fb06774115b996
basumbintwali <basumbintwali@gmail.com>
2017-11-03 16:47:19 +0100

    exercise 2.5 : improve indentation

--- Exo2/exo25.c
+++ Exo2/exo25.c
@@ -2,62 +2,33 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdbool.h>
-
-
-
 int   numb_div(int  numb) ;
 int  *divid_numb(int  numb) ;
-
-
-
 int main(int argc, char *argv[] )
-
 { 
 	char *oprd_l =argv[1];
-	
 	int numb = (int)strtol(oprd_l, NULL, 10);
 	int cpt=numb_div(numb) ;
 	int *p ; 
-
 	p= divid_numb(numb); 
 	int sum_div =numb;
-
-
 	for(int i =0 ; i<cpt ; i++)
 	{
-	     sum_div+= *p ;
-
-         p++;
-
-	     
-
-
-
-
+		sum_div+= *p ;
+		p++;
 	}
 	if(sum_div==2*numb)
 	{
 		printf("The number is a perfect") ;
-        }
-        else 
-    	        printf("The number is not a perfect number") ;
-
+	}
+	else 
+	{
+		printf("The number is not a perfect number") ;
+	}
 }
-
-
-
-
-
-/*
-Function to computer the  number of divider number
-*/
-
-
 int  numb_div(int  numb)
 {
 	int cpt=0;
-	
-
 	for(int i=1; i<numb; i++)
 	{
 		if((numb %i)==0)
@@ -66,40 +37,21 @@ int  numb_div(int  numb)
 		}
 	}
 	return cpt ;
-
-
 }
-
-/*
-Function to computer the different divider numbers
-*/
-
 int  *divid_numb(int  numb)
 {
-
 	int cpt = numb_div(numb) ;
-        
-    int  *array_div=malloc(sizeof(int)*cpt) ;
-    int j=0;
-	
+	int  *array_div=malloc(sizeof(int)*cpt) ;
+	int j=0;
 	for(int i=1; i<numb; i++)
 	{
 		if((numb %i)==0)
 		{
 			array_div[j]= i ;
 			j++;
-
 		}
-
-			
-     }
-
-
-
-
-    return array_div ;
-
-
+	}
+	return array_div ;
 }
 
 
